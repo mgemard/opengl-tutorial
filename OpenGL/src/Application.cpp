80c0da589ea901fef3b5e2d4f8b2d34b33aa5e5b
Mathieu Gemard <mgemard@asi.fr>
2018-10-18 22:20:21 +0200

    added the Texture 2D test

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -22,6 +22,9 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -52,48 +55,8 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[] = {
-		-50.0f, -50.0f, 0.0f, 0.0f, // 0
-		 50.0f, -50.0f, 1.0f, 0.0f, // 1
-		 50.0f,  50.0f, 1.0f, 1.0f, // 2
-		-50.0f,  50.0f, 0.0f, 1.0f  // 3
-	};
-
-	unsigned int indices[] = {
-		0, 1, 2, 
-		2, 3, 0
-	};
-
 	GLCall(glEnable(GL_BLEND));
 	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-	VertexArray va;
-	VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-	VertexBufferLayout layout;
-	layout.Push<float>(2);
-	layout.Push<float>(2);
-	va.AddBuffer(vb, layout);
-	
-	IndexBuffer ib(indices, 6);
-	
-	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-	//glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-	//glm::mat4 mvp = proj * view * model;
-
-	Shader shader("res/shaders/Basic.shader");
-	shader.Bind();
-	shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-
-	Texture texture("res/textures/android_robot.png");
-	texture.Bind();
-	shader.SetUniform1i("u_Texture", 0);
-
-	va.Unbind();
-	vb.Unbind();
-	ib.Unbind();
-	shader.Unbind();
 
 	Renderer renderer;
 
@@ -105,57 +68,44 @@ int main(void)
 	ImGui::StyleColorsDark();
 	//ImGui::StyleColorsClassic();
 
-	bool show_demo_window = true;
-	bool show_another_window = false;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	test::Test* currentTest = nullptr;
+	test::TestMenu* testMenu = new test::TestMenu(currentTest);
+	currentTest = testMenu;
 
-	glm::vec3 translationA(200, 200, 0);
-	glm::vec3 translationB(400, 200, 0);
+	testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+	testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
-	float r = 0.0f;
-	float increment = 0.05f;
+	test::TestClearColor test;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 0.0f));
 		renderer.Clear();
 
-		// Start the Dear ImGui frame
+		//test.OnUpdate(0.0f);
+		//test.OnRender();
+
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-
+		if (currentTest)
 		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
-			
-			renderer.Draw(va, ib, shader);
+			currentTest->OnUpdate(0.0f);
+			currentTest->OnRender();
+			ImGui::Begin("Test");
+			if (currentTest != testMenu && ImGui::Button("<-"))
+			{
+				delete currentTest;
+				currentTest = testMenu;
+			}
+			currentTest->OnImGuiRender();
+			ImGui::End();
+
 		}
-
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
-			
-			renderer.Draw(va, ib, shader);
-		}
-
-		if (r > 1.0f)
-			increment = -0.05f;
-		else if (r < 0.0f)
-			increment = 0.05f;
-
-		r += increment;
-
-		// Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-		{
-			ImGui::SliderFloat("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::SliderFloat("Translation B", &translationB.y, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
+		
+		//test.OnImGuiRender();
 
 		// Rendering
 		ImGui::Render();
@@ -167,6 +117,10 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+	delete testMenu;
+	if (currentTest != testMenu)
+		delete testMenu;
 
 	//glDeleteProgram(shader);
 
